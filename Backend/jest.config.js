4d5ed419dbef5357a603a34c53283df45a93f486
Vivan Sharma <vivans720@gmail.com>
2026-08-13 22:46:46 +0530

    CI updated

--- Backend/jest.config.js
+++ Backend/jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/tests/**/*.test.js"],
   setupFilesAfterEnv: ["<rootDir>/tests/setup.js"],
-  testTimeout: 10000, // 10 seconds timeout for spinning up the in-memory DB
+  testTimeout: 60000, // 60 seconds timeout for spinning up in-memory DB in CI
 };
